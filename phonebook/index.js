7d6109c63e6d176b30f8eb91bdea22faa9c61a30
jabss <jabrijuhinin93@gmail.com>
2024-06-07 23:56:34 +0800

    added environment variable(to deploy app to internet)

--- phonebook/index.js
+++ phonebook/index.js
@@ -11,6 +11,21 @@ app.use(express.json())
 // Define a custom token to log request body
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+// Middleware to capture response body
+// app.use((req, res, next) => {
+//   // console.log("REQUESTTT", req)
+//   console.log("RESPONSEEE", res)
+  
+//   let oldSend = res.send;
+
+//   res.send = function (data) {
+//     res.locals.body = data;
+//     // res.locals.body = data; // Save response body in res.locals
+//     oldSend.apply(res, arguments);
+//   }
+//   next();
+// });
+
 app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
@@ -18,7 +33,7 @@ app.use(morgan(function (tokens, req, res) {
     tokens.status(req, res),
     tokens.res(req, res, 'content-length', req.body), '-',
     tokens['response-time'](req, res), 'ms',
-    tokens.body(req, res)
+    tokens.body(req, res),
   ].join(' ')
 }))
 
@@ -48,31 +63,34 @@ let phonebook =
 
 
 app.get('/', (request, response) => {
-  
-    const allData = phonebook.map(data => data.name)
-    
+    // const allData = phonebook.map(data => data.name)
+    const allData = phonebook.map(data => 
+      { return `
+        <div> 
+          <p> Id : ${data.id} </p>
+          <p> Name : ${data.name} </p>
+          <p> Phonenumber : ${data.number} </p>
+        </div> 
+        <br/>`}).join()
     response.send(`${allData} <br/>`)
-
-  
 })
 
 app.get('/info', (request, response) => {
   const allData = phonebook.map(data => data.id)
   const convertToNumber = Math.max(...allData)
-
   let currentDate = moment.tz('Asia/Shanghai').format('dddd MMMM Do YYYY, h:mm:ss a') + " GMT" + moment.tz('Asia/Shanghai').format('Z') + " (" + moment.tz('Asia/Shanghai').zoneAbbr() + ")";
   response.send(`Phonebook info has ${convertToNumber} people <br/> <br/> ${currentDate}`)
 })
 
 // GET all person info
 app.get('/api/persons', (request, response) => {
-
   console.log('Response:  ', response.json)
   response.json(phonebook)
 })
 
 // GET specific people info
 app.get('/api/persons/:id', (request, response) => {
+  console.log("request", request)
   const id = Number(request.params.id)
   const fromPhonebook = phonebook.find(person => {
     return person.id === id
@@ -86,11 +104,10 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 // POST add new people
-app.post('/api/persons/', (request, response) => { 
+app.post('/api/persons/', (request, response) => {  
   const maxId = phonebook.map(person=>person.id)
   const generateId = phonebook.length > 0 ? Math.max(...maxId) + 1 : 0
   console.log(generateId)
-
 
   const body = request.body
 
@@ -126,7 +143,7 @@ app.delete('/api/persons/:id', (request,response) => {
   response.status(204).end();
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
