6aff3e1ed59750cb26ede9098b449af2e95eea3c
jabss <jabrijuhinin93@gmail.com>
2024-06-11 22:47:18 +0800

    add .env in .gitignore

--- mongo.js
+++ mongo.js
@@ -1,32 +1,48 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+// const mongoose = require('mongoose')
+
+if (process.argv.length<5) {
+  console.log('Usage: node mongo.js <password> <name> <number>');
   process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3];
+const number = process.argv[4];
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+// Replace placeholder with the actual password
+const url = process.env.MONGODB_URI.replace('<password>', password);
+
+// const url =
+//   `mongodb+srv://jabrijuhinin93:54264316426153jJ%40@cluster0.5brgeat.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
+// let name = 'Mary Poppendieck'
+// let number2 = 39236423122
+
+const person = new Person({
+  name: `${name}`,
+  number: `${number}`,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+person.save().then(result => {
+  console.log(`added ${name} number ${number} to phonebook`)
   mongoose.connection.close()
 })
