92edabe33b6534d8476468ca3406e42a60a0396f
jabss <jabrijuhinin93@gmail.com>
2024-06-15 00:07:36 +0800

    add 'runValidators' and 'context' on Person.findByIdAndUpdate

--- index.js
+++ index.js
@@ -225,7 +225,11 @@ app.put('/api/persons/:id', (request, response, next) => {
     "number": body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  Person.findByIdAndUpdate(
+    request.params.id,
+    person, 
+    { new: true , runValidators: true, context: 'query' }
+   ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
