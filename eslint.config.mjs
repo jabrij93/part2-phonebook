417536d9246215c0de5fef1162b260afe8ba841a
jabss <jabrijuhinin93@gmail.com>
2024-06-16 19:02:34 +0800

    configured eslint correctly, some issues with import and export but still working

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from 'globals';
+import stylisticJsPlugin from '@stylistic/eslint-plugin-js';
+
+// Manually specify recommended rules (a simplified example)
+const recommendedRules = {
+  // Add other recommended rules here
+  'no-unused-vars': 'error',
+  'no-undef': 'error',
+  'eqeqeq': 'error',
+  'no-trailing-spaces': 'error',
+  'object-curly-spacing': ['error', 'always'],
+  'arrow-spacing': ['error', { 'before': true, 'after': true }],
+  'no-console': 0
+};
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.browser,
+    },
+    plugins: {
+      '@stylistic/js': stylisticJsPlugin,
+    },
+    rules: {
+      ...recommendedRules,
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+];
